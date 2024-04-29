8dc20c63efb29f0264b2a2b73e161a5ab4c67c48
PascalZh <1315521905@qq.com>
2024-04-29 15:52:39 +0800

    修改API，使用scipy库Rotation表示旋转，修复cx和center不一致的问题

--- bbox/bbox3d.py
+++ bbox/bbox3d.py
@@ -5,7 +5,7 @@
 from copy import deepcopy
 
 import numpy as np
-from pyquaternion import Quaternion
+from scipy.spatial.transform import Rotation
 
 
 class BBox3D:
@@ -24,42 +24,36 @@ class BBox3D:
         length (:py:class:`float`, optional): The length of the box (default is 1).
         width (:py:class:`float`, optional): The width of the box (default is 1).
         height (:py:class:`float`, optional): The height of the box (default is 1).
-        rw (:py:class:`float`, optional): The real part of the rotation quaternion (default is 1).
         rx (:py:class:`int`, optional): The first element of the quaternion vector (default is 0).
         ry (:py:class:`int`, optional): The second element of the quaternion vector (default is 0).
         rz (:py:class:`int`, optional): The third element of the quaternion vector (default is 0).
+        rw (:py:class:`float`, optional): The real part of the rotation quaternion (default is 1).
         euler_angles (:py:class:`list` or :py:class:`ndarray` of float, optional): Sequence of \
-            euler angles in `[x, y, z]` rotation order (the default is None).
+            euler angles in zyx rotation order (the default is None).
         is_center (`bool`, optional): Flag to indicate if the provided coordinate is the \
             center of the box (the default is True).
     """
 
     def __init__(self, x, y, z,
                  length=1, width=1, height=1,
-                 rw=1, rx=0, ry=0, rz=0, q=None,
+                 rx=0, ry=0, rz=0, rw=1, q=None,
                  euler_angles=None, is_center=True):
         if is_center:
-            self._cx, self._cy, self._cz = x, y, z
             self._c = np.array([x, y, z])
         else:
-            self._cx = x + length/2
-            self._cy = y + width/2
-            self._cz = z + height/2
-            self._c = np.array([self._cx, self._cy, self._cz])
+            self._c = np.array([x+length/2, y+width/2, z+height/2])
 
         self._w, self._h, self._l = width, height, length
 
         if euler_angles:
             # we need to apply y, z and x rotations in order
             # http://www.euclideanspace.com/maths/geometry/rotations/euler/index.htm
-            self._q = Quaternion(axis=[0, 1, 0], angle=euler_angles[1]) * \
-                Quaternion(axis=[0, 0, 1], angle=euler_angles[2]) * \
-                Quaternion(axis=[1, 0, 0], angle=euler_angles[0])
+            self._rotate = Rotation.from_euler('zyx', euler_angles)
 
         elif q is not None:
-            self._q = Quaternion(q)
+            self._rotate = Rotation.from_quat(q)
         else:
-            self._q = Quaternion(rw, rx, ry, rz)
+            self._rotate = Rotation.from_quat([rx, ry, rz, rw])
 
     @property
     def center(self):
@@ -80,7 +74,7 @@ class BBox3D:
     def center(self, c):
         if len(c) != 3:
             raise ValueError("Center coordinates should be a vector of size 3")
-        self._c = c
+        self._c[:] = c
 
     def __valid_scalar(self, x):
         if not np.isscalar(x):
@@ -95,33 +89,33 @@ class BBox3D:
         """
         :py:class:`float`: X coordinate of center.
         """
-        return self._cx
+        return self._c[0]
 
     @cx.setter
     def cx(self, x):
-        self._cx = self.__valid_scalar(x)
+        self._c[0] = self.__valid_scalar(x)
 
     @property
     def cy(self):
         """
         :py:class:`float`: Y coordinate of center.
         """
-        return self._cy
+        return self._c[1]
 
     @cy.setter
     def cy(self, x):
-        self._cy = self.__valid_scalar(x)
+        self._c[1] = self.__valid_scalar(x)
 
     @property
     def cz(self):
         """
         :py:class:`float`: Z coordinate of center.
         """
-        return self._cz
+        return self._c[2]
 
     @cz.setter
     def cz(self, x):
-        self._cz = self.__valid_scalar(x)
+        self._c[2] = self.__valid_scalar(x)
 
     @property
     def q(self):
@@ -131,17 +125,17 @@ class BBox3D:
         Returns
             :py:class:`ndarray` of float: Quaternion values in (w, x, y, z) form.
         """
-        return np.hstack((self._q.real, self._q.imaginary))
+        return self._rotate.as_quat()
 
     @q.setter
     def q(self, q):
-        if not isinstance(q, (list, tuple, np.ndarray, Quaternion)):
+        if not isinstance(q, (list, tuple, np.ndarray)):
             raise TypeError(
-                "Value shoud be either list, numpy array or Quaterion")
+                "Value shoud be either list, numpy array")
         if isinstance(q, (list, tuple, np.ndarray)) and len(q) != 4:
             raise ValueError("Quaternion input should be a vector of size 4")
 
-        self._q = Quaternion(q)
+        self._rotate = Rotation.from_quat(q)
 
     @property
     def quaternion(self):
@@ -156,6 +150,16 @@ class BBox3D:
     @quaternion.setter
     def quaternion(self, q):
         self.q = q
+
+    @property
+    def rotation(self):
+        return self._rotate
+
+    @rotation.setter
+    def rotation(self, r):
+        if not isinstance(r, Rotation):
+            raise TypeError("Rotation should be a scipy.spatial.transform.Rotation object")
+        self._rotate = r
 
     @property
     def l(self):
@@ -223,11 +227,18 @@ class BBox3D:
     def height(self, x):
         self.h = x
 
-    def __transform(self, x):
+    def transform(self, x):
         """
         Rotate and translate the point to world coordinates.
         """
-        y = self._c + self._q.rotate(x)
+        y = self._c + self._rotate.apply(x)
+        return y
+
+    def transform_inverse(self, x):
+        """
+        Rotate and translate the point to local coordinates.
+        """
+        y = self._rotate.inv().apply(x - self._c)
         return y
 
     @property
@@ -236,7 +247,7 @@ class BBox3D:
         :py:class:`float`: Back-left-bottom point.
         """
         p = np.array([-self._l/2, -self._w/2, -self._h/2])
-        p = self.__transform(p)
+        p = self.transform(p)
         return p
 
     @property
@@ -245,7 +256,7 @@ class BBox3D:
         :py:class:`float`: Front-left-bottom point.
         """
         p = np.array([self._l/2, -self._w/2, -self._h/2])
-        p = self.__transform(p)
+        p = self.transform(p)
         return p
 
     @property
@@ -254,7 +265,7 @@ class BBox3D:
         :py:class:`float`: Front-right-bottom point.
         """
         p = np.array([self._l/2, self._w/2, -self._h/2])
-        p = self.__transform(p)
+        p = self.transform(p)
         return p
 
     @property
@@ -263,7 +274,7 @@ class BBox3D:
         :py:class:`float`: Back-right-bottom point.
         """
         p = np.array([-self._l/2, self._w/2, -self._h/2])
-        p = self.__transform(p)
+        p = self.transform(p)
         return p
 
     @property
@@ -272,7 +283,7 @@ class BBox3D:
         :py:class:`float`: Back-left-top point.
         """
         p = np.array([-self._l/2, -self._w/2, self._h/2])
-        p = self.__transform(p)
+        p = self.transform(p)
         return p
 
     @property
@@ -281,7 +292,7 @@ class BBox3D:
         :py:class:`float`: Front-left-top point.
         """
         p = np.array([self._l/2, -self._w/2, self._h/2])
-        p = self.__transform(p)
+        p = self.transform(p)
         return p
 
     @property
@@ -290,7 +301,7 @@ class BBox3D:
         :py:class:`float`: Front-right-top point.
         """
         p = np.array([self._l/2, self._w/2, self._h/2])
-        p = self.__transform(p)
+        p = self.transform(p)
         return p
 
     @property
@@ -299,7 +310,7 @@ class BBox3D:
         :py:class:`float`: Back-right-top point.
         """
         p = np.array([-self._l/2, self._w/2, self._h/2])
-        p = self.__transform(p)
+        p = self.transform(p)
         return p
 
     @property
@@ -316,12 +327,10 @@ class BBox3D:
 
     def __repr__(self):
         template = "BBox3D(x={cx}, y={cy}, z={cz}), length={l}, width={w}, height={h}, "\
-            "q=({rw}, {rx}, {ry}, {rz}))"
+            "q={q})"
         return template.format(
-            cx=self._cx, cy=self._cy, cz=self._cz,
-            l=self._l, w=self._w, h=self._h,
-            rw=self._q.real,
-            rx=self._q.imaginary[0], ry=self._q.imaginary[1], rz=self._q.imaginary[2])
+            cx=self.cx, cy=self.cy, cz=self.cz,
+            l=self._l, w=self._w, h=self._h, q=self._rotate.as_quat().tolist())
 
     def copy(self):
         return deepcopy(self)
